e43094d44a354c606cfab115aaf9e69c7b5a1f48
Ted Conbeer <tconbeer@users.noreply.github.com>
2023-05-03 21:14:20 -0600

    fix: stop input line cropping

--- src/harlequin/tui/components/code_editor.py
+++ src/harlequin/tui/components/code_editor.py
@@ -1,6 +1,9 @@
 from sqlfmt.api import Mode, format_string
 from textual.binding import Binding
 from textual.widgets import Input
+from rich.highlighter import Highlighter
+from rich.segment import Segment
+from textual.widgets._input import _InputRenderable
 
 from harlequin.tui.components.filename_modal import FilenameModal
 
@@ -15,6 +18,32 @@ class CodeEditor(Input):
         ("ctrl+o", "load", "Open Query"),
         Binding("enter", "newline", "", show=False),
     ]
+
+    def __init__(
+        self,
+        value: str | None = None,
+        placeholder: str = "",
+        highlighter: Highlighter | None = None,
+        password: bool = False,
+        name: str | None = None,
+        id: str | None = None,
+        classes: str | None = None,
+        disabled: bool = False,
+    ) -> None:
+        # The default input widget crops the input when it gets too long.
+        # We don't want this! This monkeypatches the method so it doesn't
+        # do any cropping
+        def line_crop(  # type: ignore
+            segments: list[Segment],
+            *args,
+            **kwargs,
+        ) -> list[Segment]:
+            return segments
+
+        _InputRenderable.__rich_console__.__globals__["line_crop"] = line_crop
+        super().__init__(
+            value, placeholder, highlighter, password, name, id, classes, disabled
+        )
 
     def on_mount(self) -> None:
         self.border_title = "Query Editor"
