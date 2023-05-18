36e9a9792382e09317b9fcb8510bbb473d759339
Ted Conbeer <tconbeer@users.noreply.github.com>
2023-05-18 15:28:59 -0600

    feat: add read-only option

--- src/harlequin/cli.py
+++ src/harlequin/cli.py
@@ -7,12 +7,19 @@ from harlequin import Harlequin
 
 @click.command()
 @click.version_option(package_name="harlequin")
+@click.option(
+    "-r",
+    "-readonly",
+    "--read-only",
+    is_flag=True,
+    help="Open the database file in read-only mode.",
+)
 @click.argument(
     "db_path",
     default=":memory:",
     nargs=1,
     type=click.Path(path_type=Path),
 )
-def harlequin(db_path: Path) -> None:
-    tui = Harlequin(db_path=db_path)
+def harlequin(db_path: Path, read_only: bool) -> None:
+    tui = Harlequin(db_path=db_path, read_only=read_only)
     tui.run()
