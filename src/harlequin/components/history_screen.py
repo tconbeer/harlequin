c0991fb36150e309acd334b144720945739c3847
Ted Conbeer <tconbeer@users.noreply.github.com>
2024-12-19 15:54:38 -0700

    fix: refactor history screen to remove pygments deps

--- src/harlequin/components/history_screen.py
+++ src/harlequin/components/history_screen.py
@@ -1,24 +1,79 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, ClassVar
+
+from rich.columns import Columns
+from rich.console import Group, RenderableType
 from rich.padding import Padding
-from rich.syntax import Syntax
+from rich.style import Style
+from rich.text import Text
 from textual import on
 from textual.app import ComposeResult
-from textual.containers import Horizontal, VerticalScroll
+from textual.containers import Horizontal
 from textual.screen import Screen
-from textual.widgets import OptionList, Static
+from textual.widgets import OptionList
 from textual.widgets.option_list import Option
+from textual_textarea import TextEditor
 
 from harlequin.history import History, QueryExecution
 from harlequin.messages import WidgetMounted
+
+if TYPE_CHECKING:
+    from textual.app import RenderResult
 
 
 class HistoryOption(Option):
     PADDING = (0, 1, 0, 1)
 
-    def __init__(self, item: QueryExecution) -> None:
-        super().__init__(prompt=Padding(item, pad=self.PADDING))
+    def __init__(self, item: QueryExecution, error_style: Style) -> None:
+        super().__init__(prompt="")
+        self.item = item
+        self.error_style = error_style
         self.value = item.query_text
+
+    @property
+    def prompt(self) -> RenderResult:
+        """The prompt for the option."""
+        ts = self.item.executed_at.strftime("%a, %b %d %H:%M:%S")
+        if self.item.result_row_count < 0:
+            result = Text("ERROR", style=self.error_style, justify="right")
+        else:
+            res = (
+                f"{self.item.result_row_count:n} "
+                f"{'record' if self.item.result_row_count == 1 else 'records'}"
+                if self.item.result_row_count
+                else "SUCCESS"
+            )
+            elapsed = f"{self.item.elapsed:.2f}s"
+            result = Text.assemble(
+                (res, "bold"), " in ", (elapsed, "bold"), justify="right"
+            )
+        query_lines = self.item.query_text.strip().splitlines()
+        if len(query_lines) > 8:
+            continuation: RenderableType = Text(
+                f"… ({len(query_lines) - 7} more lines)\n", style="italic"
+            )
+            query_lines = query_lines[0:7]
+        else:
+            continuation = ""
+
+        return Padding(
+            Group(
+                Columns(
+                    renderables=[Text(ts, style="bold"), result],
+                    expand=True,
+                ),
+                "\n".join(query_lines),
+                continuation,
+            ),
+            pad=self.PADDING,
+        )
+
+    def __rich__(self) -> RenderResult:
+        return self.prompt
+
+    def visualize(self) -> object:
+        return self.prompt
 
 
 class HistoryList(OptionList):
@@ -27,6 +82,10 @@ class HistoryList(OptionList):
 
 
 class HistoryScreen(Screen[str]):
+    COMPONENT_CLASSES: ClassVar[set[str]] = {
+        "history-screen--error-label",
+    }
+
     def __init__(
         self,
         history: History,
@@ -40,16 +99,24 @@ class HistoryScreen(Screen[str]):
         self.theme = theme
 
     def compose(self) -> ComposeResult:
-        self.list = HistoryList(*reversed([HistoryOption(q) for q in self.history]))
-        self.preview = Static("")
+        error_style = self.get_component_rich_style("history-screen--error-label")
+        error_text_style = Style(
+            color=error_style.color, italic=error_style.italic, bold=error_style.bold
+        )
+        self.list = HistoryList(
+            *reversed(
+                [HistoryOption(q, error_style=error_text_style) for q in self.history]
+            )
+        )
+        self.preview = TextEditor(
+            language="sql", theme=self.theme, read_only=True, use_system_clipboard=False
+        )
         with Horizontal():
             yield self.list
-            with VerticalScroll():
-                yield self.preview
+            yield self.preview
 
     def on_mount(self) -> None:
-        scroll = self.query_one(VerticalScroll)
-        scroll.border_title = "Highlighted Query Preview"
+        self.preview.border_title = "Highlighted Query Preview"
         self.post_message(WidgetMounted(widget=self))
 
     def action_cancel(self) -> None:
@@ -70,11 +137,4 @@ class HistoryScreen(Screen[str]):
         message.stop()
         query = getattr(message.option, "value", None)
         assert isinstance(query, str)
-        self.preview.update(
-            Syntax(
-                code=query,
-                lexer="sql",
-                theme=self.theme,
-                line_numbers=True,
-            )
-        )
+        self.preview.text = query
